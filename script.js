5a0689e5305b5ba0d2ceca0d12f54e2cbad9372f
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-12-01 13:47:11 +0100

    done

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const form = document.querySelector(".purmo__pdf--form");
 const downloadBtn = document.getElementById("download-pdf-btn");
 const errorText = document.getElementById("validation-text");
 const input = document.getElementsByClassName("purmo__pdf--input-field")[0];
-
-console.log(input)
+const select = document.querySelector('select[name="item-code"');
 
 // item codes: F4XA017006000300, FCT2206006010000
 
-// ******************* Event handler *******************
+// ******************* Event handler ******************* //
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,20 +28,16 @@ form.addEventListener("submit", (e) => {
     .catch((err) => console.log(err));
 });
 
-// ******************* Main functions *******************
+// ******************* Main functions ******************* //
 
 function handleInputSuccess(url) {
   addClass(errorText, 'd-none'); // hide validation text
-  normalizeBorderAppearance(input); // removes previous border appearance class
-  addClass(input, 'is-valid'); // display green border around input
   removeClass(downloadBtn, 'd-none'); // display download button
   setHrefAttribute(downloadBtn, url); // set href to pdf-url on button
-}
+};
 
 function handleInputError() {
   removeClass(errorText, 'd-none'); // display validation text
-  normalizeBorderAppearance(input); // removes previous border appearance class 
-  addClass(input, 'is-invalid'); // display red border around input
   addClass(downloadBtn, 'd-none'); // hide download button
 };
 
@@ -55,14 +50,10 @@ function filterData(data, language, itemCode) {
     .find(x => x.ItemCode.toLowerCase() === item);
 };
 
-// ******************* Helper functions *******************
+// ******************* Helper functions ******************* //
 
 function setHrefAttribute(element, url) {
   element.setAttribute("href", url);
-};
-
-function toggleVisibility(element, boolean) {
-  element.classList.toggle('d-none', boolean);
 };
 
 function addClass(element, className) {
@@ -71,14 +62,4 @@ function addClass(element, className) {
 
 function removeClass(element, className) {
   element.classList.remove(className);
-};
-
-function normalizeBorderAppearance(input) {
-  const CLASSNAMES = ['is-valid', 'is-invalid'];
-
-  CLASSNAMES.forEach(className => {
-    if (input.classList.contains(className)) {
-      removeClass(input, className);
-    };
-  });
 };
